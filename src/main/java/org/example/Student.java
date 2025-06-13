6af9ac7009cf9130710f0c508799808f546afcd8
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-06-13 21:53:13 +0530

    Student Entity Called by App Class

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,7 +1,34 @@
 package org.example;
 
-public class Student {
-    private int id;
-    private String name;
-    private String city;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.SessionFactory;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+
+@Entity
+public class Student{
+    @Id
+    private int id; // Treated as column name in Table
+
+    @Column(length=20)
+    private float marks; // Treated as column name in Table
+
+    @Column(length=20)
+    private String name; // Treated as column name in Table
+
+    public Student(){ // Default Constructor
+
+    }
+
+    public Student(int id, float marks, String name){ // For value assign
+        this.id= id;
+        this.marks= marks;
+        this.name= name;
+    }
+
 }
+
