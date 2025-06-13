605977d2ef7f770ae868f8267ed358648654511c
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-06-13 21:52:42 +0530

    App By Default Java Class

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,19 +1,28 @@
 package org.example;
 
-import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-
+import org.hibernate.SessionFactory;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println("Project Started.." );
+
+        System.out.println("Project Started..");
 
         Configuration cfg= new Configuration();
         cfg.configure("hibernate.cfg.xml");
         SessionFactory factory= cfg.buildSessionFactory();
-        System.out.println(factory);
-        System.out.println(factory.isClosed());
+
+        Student s1= new Student(004, 9.2f, "Nitish");
+        Session session= factory.openSession();
+        Transaction tx= session.beginTransaction();
+        session.save(s1);
+
+        tx.commit();
+        session.close();
+
     }
 }
