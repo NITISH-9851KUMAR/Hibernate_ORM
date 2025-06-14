2e9065bd8304647f62692a246c7253ccf6b8a453
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-06-14 20:16:44 +0530

    Student Entity

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Column;
@@ -10,13 +11,26 @@ public class Student{
     private int id; // Treated as column name in Table
 
     @Column(length=20)
-    private float marks; // Treated as column name in Table
+    private double marks; // Treated as column name in Table
 
     @Column(length=20)
     private String name; // Treated as column name in Table
 
-    public Student(){ // Default Constructor
+    private Certificate certificate; // Declare Certificate Object
 
+    public Certificate getCertificate(){
+        return certificate;
+    }
+
+    public void setCertificate(Certificate certificate){
+        this.certificate= certificate;
+    }
+
+    public Student(){} // Default Constructor
+
+    public String toString(){
+        float marks1= (int)marks;
+        return id+" "+name+" "+marks1;
     }
 
     public Student(int id, float marks, String name){ // For value assign
