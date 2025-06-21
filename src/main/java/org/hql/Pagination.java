bfa557e320121530a5c891cbb69c05efb1017474
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-06-21 10:50:58 +0530

    Fetch Value between Range

--- src/main/java/org/hql/Pagination.java
+++ src/main/java/org/hql/Pagination.java
@@ -6,23 +6,28 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 import javax.persistence.Query;
+import java.util.List;
 
-public class Update {
+public class Pagination {
     public static void main(String[] args) {
+
         Configuration cfg= new Configuration();
         cfg.configure("hibernate2.cfg.xml");
         SessionFactory factory= cfg.buildSessionFactory();
         Session session= factory.openSession();
 
-        Transaction tx= session.beginTransaction();
-
-        // Delete value for table
-        String q= "UPDATE Student set name='Nitish Kumar' WHERE id= 104";
+        String q= "FROM Student";
         Query query= session.createQuery(q);
-        int row= query.executeUpdate();
-        System.out.println("Row Updated: "+row);
 
-        tx.commit();
+        // implementing pagination
+        // pagination-> means print the result between a range value
+
+        query.setFirstResult(4); // starting point
+        query.setMaxResults(2); // Number of value(result) basically how much value we want
+
+        List<Student> s= query.getResultList();
+        for(Student list: s)
+            System.out.println(list);
 
         session.close();
         factory.close();
