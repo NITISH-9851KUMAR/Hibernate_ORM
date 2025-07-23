3ae395e44dbe39037f08384f602bb3ff2555d76c
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-07-23 17:31:18 +0530

    Hibernate Basics Annotation

---
+++ src/main/java/np/Annotation.java
@@ -0,0 +1,123 @@
+package np;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Transient;
+import javax.persistence.Lob;
+import javax.persistence.Temporal;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.TemporalType;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Date;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.Transaction;
+
+@Entity
+@Table(name= "student_add")
+public class Annotation {
+    @Id
+    @Column(name= "address")
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int addressId;
+
+    @Column(length= 5, name= "street_no")
+    private int street;
+
+    @Column(name= "city_add", length= 20)
+    private String city;
+
+    @Column(name= "is_open", length= 5)
+    private boolean isOpen;
+
+    @Transient
+    private double x;
+
+    @Temporal(TemporalType.DATE)
+    private Date addDate;
+
+    @Lob
+    private byte[] image;
+
+    public Annotation(){
+        super();
+    }
+
+    public Annotation(int street, String city, boolean isOpen) {
+        this.addressId = addressId;
+        this.street = street;
+        this.city = city;
+        this.isOpen = isOpen;
+        this.x = x;
+        this.addDate = addDate;
+        this.image = image;
+    }
+
+    public int getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(int addressId) {
+        this.addressId = addressId;
+    }
+
+    public int getStreet() {
+        return street;
+    }
+
+    public void setStreet(int street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public boolean isOpen() {
+        return isOpen;
+    }
+
+    public void setOpen(boolean open) {
+        isOpen = open;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public Date getAddDate() {
+        return addDate;
+    }
+
+    public void setAddDate(Date addDate) {
+        this.addDate = addDate;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public String toString(){
+        return addressId+" "+street+" "+city;
+    }
+
+}
