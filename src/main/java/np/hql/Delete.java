164642c4a3ca6303a0fd0b02b5799b8b23e99f66
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-07-24 11:37:08 +0530

    Detlete value HQL

--- src/main/java/np/hql/Delete.java
+++ src/main/java/np/hql/Delete.java
@@ -1,4 +1,33 @@
 package np.hql;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import javax.persistence.Query;
+
 public class Delete {
+    public static void main(String[] args) {
+
+        Configuration cfg = new Configuration();
+        cfg.configure("hibernate3.cfg.xml");
+        SessionFactory factory = cfg.buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction tx= session.beginTransaction();
+
+        String str= "DELETE FROM Student WHERE grade= :grade";
+        Query query= session.createQuery(str);
+        query.setParameter("grade", 'E');
+
+        // this method execute the query and it returns integer value
+        int rowAffect= query.executeUpdate();
+        System.out.println("Deleted Successfully : "+rowAffect);
+
+        tx.commit();
+
+        session.close();
+        factory.close();
+
+    }
 }
