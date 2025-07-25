b61419612b35ecae2584a7db3f5e6df05b45ca86
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-07-25 11:28:20 +0530

    App Embeddable Annotation

--- src/main/java/org/EmbeddedAnno/App.java
+++ src/main/java/org/EmbeddedAnno/App.java
@@ -5,26 +5,43 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import javax.persistence.Query;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
 
         Configuration cfg = new Configuration();
-        cfg.configure("hibernate3.cfg.xml");
+        cfg.configure("hibernate_mysql.cfg.xml");
         SessionFactory factory = cfg.buildSessionFactory();
         Session session = factory.openSession();
         Transaction tx = session.beginTransaction();
 
-        Person p1 = new Person(1, "ABC", "Phulera");
-        Person p2 = new Person(2, "DEF", "Fakauli");
+//        PersonE p1 = new PersonE(1, "ABC", "Phulera");
+//        PersonE p2 = new PersonE(2, "DEF", "Fakauli");
+//
+//        Work w1 = new Work("Police-Paint", "8 hours");
+//        Work w2 = new Work("Painter", "8 hours");
+//
+//        p1.setWork(w1);
+//        p2.setWork(w2);
+//
+//        session.save(p1);
+//        session.save(p2);
 
-        Work w1 = new Work("Police-Paint", "8 hours");
-        Work w2 = new Work("Painter", "8 hours");
+        // Session Api Fetch Technique
+//        PersonE person= session.get(PersonE.class, 1);
+//        System.out.println(person);
+//        System.out.println(person.getWork());
 
-        p1.setWork(w1);
-        p2.setWork(w2);
-
-        session.save(p1);
-        session.save(p2);
+        // HQL Fetch Technique
+//        String hql= "FROM PersonE";
+//        Query query= session.createQuery(hql);
+//        List<PersonE> list= query.getResultList();
+//        for(PersonE e: list){
+//            System.out.println(e);
+//            System.out.println(e.getWork());
+//        }
 
         tx.commit();
 
