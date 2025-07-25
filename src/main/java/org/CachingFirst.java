4b5bf1834370129dc8315537e71bfcf9633eae93
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-07-25 17:07:03 +0530

    First Level Cache Session Associated

---
+++ src/main/java/org/CachingFirst.java
@@ -0,0 +1,29 @@
+package org;
+
+import org.hibernate.*;
+import org.hibernate.cfg.Configuration;
+
+public class CachingFirst {
+    public static void main(String[] args) {
+
+        Configuration cfg= new Configuration();
+        cfg.configure("hibernate_mysql.cfg.xml");
+        SessionFactory factory= cfg.buildSessionFactory();
+        Session session= factory.openSession();
+
+        Student s= session.get(Student.class, 31004);
+        System.out.println(s);
+
+        System.out.println("Working Something.....");
+
+        // Use First Level Caching because we fetch data for same object
+        // So hibernate first check if same object present in cache memory then they don't hit database again
+        // It is First level caching and valid till session.close(); method
+        Student s1= session.get(Student.class, 31004);
+        System.out.println(s1);
+
+        session.close();
+        factory.close();
+
+    }
+}
