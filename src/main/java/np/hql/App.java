b1c6bef4b726c6c175678d2fc575a8dcd3b77a5e
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-07-25 09:40:36 +0530

    Deleted File From org package

---
+++ src/main/java/np/hql/App.java
@@ -0,0 +1,34 @@
+package np.hql;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.*;
+
+import org.hql.Student;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+
+        Configuration cfg = new Configuration();
+        cfg.configure("hibernate3.cfg.xml");
+        SessionFactory factory = cfg.buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction tx = session.beginTransaction();
+
+        String sql= "SELECT * FROM student";
+        NativeQuery nq= session.createNativeQuery(sql);
+        List<Object[]> list= nq.getResultList();
+        for(Object[] l: list){
+            System.out.println(Arrays.toString(l));
+        }
+
+        session.close();
+        factory.close();
+
+    }
+}
