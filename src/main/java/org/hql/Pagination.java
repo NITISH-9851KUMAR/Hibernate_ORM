b3138ee3d8d313d8600f86a5f90261c67a724e1a
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-07-25 11:08:28 +0530

    Print Selected Value using Pagination

--- src/main/java/org/hql/Pagination.java
+++ src/main/java/org/hql/Pagination.java
@@ -1,38 +1,37 @@
-//package org.hql;
-//
-//import org.hibernate.Session;
-//import org.hibernate.SessionFactory;
-//import org.hibernate.Transaction;
-//import org.hibernate.cfg.Configuration;
-//
-//import javax.persistence.Query;
-//import java.util.List;
-//
-//import org.hql.Student;
-//
-//public class Pagination {
-//    public static void main(String[] args) {
-//
-//        Configuration cfg = new Configuration();
-//        cfg.configure("hibernate_mysql.cfg.xml");
-//        SessionFactory factory = cfg.buildSessionFactory();
-//        Session session = factory.openSession();
-//        Transaction tx = session.beginTransaction();
-//
-//        String hql = "FROM Student";
-//        Query query = session.createQuery(hql);
-//
-//        // Pagination
-//        query.setFirstResult(3); // set first position
-//        query.setMaxResults(3);  // Number of Object
-//
-//        List<Student> results = query.getResultList();
-//        for (Student student : results) {
-//            System.out.println(student);
-//        }
-//
-//        session.close();
-//        factory.close();
-//
-//    }
-//}
+package org.hql;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.*;
+import java.util.List;
+
+import org.Student;
+
+public class Pagination {
+    public static void main(String[] args) {
+
+        Configuration cfg = new Configuration();
+        cfg.configure("hibernate_mysql.cfg.xml");
+        SessionFactory factory = cfg.buildSessionFactory();
+        Session session = factory.openSession();
+        Transaction tx = session.beginTransaction();
+
+        String hql = "FROM Student";
+        Query query = session.createQuery(hql);
+
+        // Pagination
+        query.setFirstResult(3); // set first position
+        query.setMaxResults(3);  // Number of Object
+
+        List<Student> results = query.getResultList();
+        for (Student student : results) {
+            System.out.println(student);
+        }
+
+        session.close();
+        factory.close();
+
+    }
+}
